62ba0f0f7f08d9dcead467671263b3e5409e8100
Miguel Teran <miguel.teranj02@gmail.com>
2023-08-15 16:11:16 -0400

    ConsultaEnLinea_Form: Terminé de darle interactividad a los pasos en donde el usuario debe subir las imágenes de su rostro

--- src/components/ConsultaOnline/Formulario/UploadPicture.jsx
+++ src/components/ConsultaOnline/Formulario/UploadPicture.jsx
@@ -1,14 +1,35 @@
 import PropTypes from 'prop-types';
+import { useState } from 'react';
 import { NextNavButton } from '../../NextNavButton';
+
+// Font Awesome
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faHandPointRight } from '@fortawesome/free-solid-svg-icons';
 import { faUpload } from '@fortawesome/free-solid-svg-icons';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faRotateRight } from '@fortawesome/free-solid-svg-icons';
 
 export const UploadPicture = (props) => {
-	const { title, description, imgSrc, nextStep, nextButtonText } = props;
+	const { title, description, nextStep, nextButtonText } = props;
+
+	const [selectedImage, setSelectedImage] = useState(null);
+	const [showIcon, setShowIcon] = useState(true);
+	const [changeImage, setChangeImage] = useState(false);
+
+	const handleImageChange = (e) => {
+		const file = e.target.files[0];
+		if (file) {
+			const reader = new FileReader();
+			reader.onload = () => {
+				setSelectedImage(reader.result);
+				setShowIcon(false);
+				setChangeImage(true);
+			};
+			reader.readAsDataURL(file);
+		}
+	};
 
 	return (
-		<form className='grid justify-items-center max-w-[900px] mx-auto px-5'>
+		<form className='grid justify-items-center max-w-[900px] mx-auto px-5 '>
 			<h2 className='text-md text-center text-white font-bold font-my-montserrat uppercase'>
 				{title}
 			</h2>
@@ -19,18 +40,46 @@ export const UploadPicture = (props) => {
 			<p className='text-sm text-white text-center font-my-sans pb-4'>
 				Haz click para subir tu foto: ⬇️
 			</p>
-			<div className='relative'>
-				<FontAwesomeIcon
-					icon={faUpload}
-					className='absolute z-20 inset-0 m-auto text-[40px] text-white'
-					bounce
+			<div
+				onClick={() => document.getElementById('imageInput').click()}
+				className='relative cursor-pointer'>
+				<input
+					id='imageInput'
+					type='file'
+					accept='image/*'
+					className='hidden'
+					onChange={handleImageChange}
 				/>
+				{showIcon && (
+					<FontAwesomeIcon
+						icon={faUpload}
+						className='absolute z-20 inset-0 m-auto text-[40px] text-white'
+						bounce
+					/>
+				)}
 				<img
-					className='w-full max-w-[300px] h-full brightness-50'
-					src={imgSrc}
+					className={`w-full max-w-[235px] h-fit max-h-[295px] ${
+						selectedImage ? '' : 'brightness-50'
+					}`}
+					src={selectedImage || props.imgSrc}
 					alt='Upload a picture of your face'
 				/>
 			</div>
+			{changeImage ? (
+				<p
+					onClick={() =>
+						document.getElementById('imageInput').click()
+					}
+					className='text-sm text-center font-my-sans pt-4 underline cursor-pointer'>
+					Subir otra imagen
+					<FontAwesomeIcon
+						className='ml-2'
+						icon={faRotateRight}
+					/>
+				</p>
+			) : (
+				''
+			)}
 
 			<div className='flex justify-center mt-8'>
 				<NextNavButton
